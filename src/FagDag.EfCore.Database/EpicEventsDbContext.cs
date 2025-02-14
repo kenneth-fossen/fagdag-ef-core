097e5895b8349836b794944a6612fb664b6cbdff
Kenneth Fossen <kenneth.fossen@bouvet.no>
2025-02-14 15:20:26 +0100

    Minor updates

--- src/FagDag.EfCore.Database/EpicEventsDbContext.cs
+++ src/FagDag.EfCore.Database/EpicEventsDbContext.cs
@@ -4,11 +4,8 @@ namespace FagDag.EfCore.Database;
 
 public class EpicEventsDbContext(DbContextOptions<EpicEventsDbContext> options) : DbContext(options)
 {
-    private static string DbPath { get => "EpicEvents.db"; }
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
